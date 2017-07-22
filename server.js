4a570089812b9c624c435f1d05bfadd7ac5b5fa2
Piotr Majkrzak <petrol.91@gmail.com>
2017-07-22 19:40:59 +0200

    podstawowa logika serwera

--- server.js
+++ server.js
@@ -4,28 +4,54 @@ orders = [];
 
 /*
 	{
-		title: 'adasda',
-		maker: 'Piotr Majkrzak',
-		description: 'adiasda \n aisjdfaijd\n',
-		
+		author: 'Piotr Majkrzak',
+		content: 'Kebab z OzUrfa'
+		closes: '1123',
+		items: [
+			{
+				author: 'Piotr Majkrzak'
+				content: 'bółka kurczak czosnek'
+			}
+		]
 	}
-
 */
 
 const key = require('fs').readFileSync('./pub.pem');
 const client = require('fs').readFileSync('./client.html');
 
-var io = require('socket.io')(require('http').createServer((req, res) => {
-	res.writeHead(200);
-	res.end(client);
-}).listen(8080));
+require('socket.io')(require('http').createServer((req, res) => {
+		res.writeHead(200);
+		res.end(client);
+	}).listen(8080))
 
-io.use((socket, next) => {
-	require('jsonwebtoken').verify(socket.handshake.query.token, key, function(err, decoded) {
-		if (!err) {
-			socket.user = decoded;
-			return next();
-		} else
-			return next(new Error('authentication error'));
+	.use((socket, next) => {
+		require('jsonwebtoken').verify(socket.handshake.query.token, key, function(err, decoded) {
+			if (!err) {
+				socket.user = decoded;
+				return next();
+			} else
+				return next(new Error('authentication error'));
+		});
+	})
+
+	.on('connect', (socket) => {
+		socket.server.emit('update', orders);
+		socket.on('open', (content, closes) => {
+			orders.push({
+				author: socket.user,
+				connect: content,
+				closes: closes,
+				items: []
+			});
+			socket.server.emit('update', orders);
+		});
+		socket.on('take', (idx, content) => {
+			if (a[idx] && a[idx].closes > Date.now()) {
+				a[idx].items.push({
+					author: socket.user,
+					content: connect
+				});
+			};
+			socket.server.emit('update', orders);
+		});
 	});
-});
